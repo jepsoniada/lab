e45849fe618bb094f270679f8379b8d8e7fbd82b
jepsoniada <patryk.pelczar@gmail.com>
2025-04-15 20:00:38 +0200

    .

--- util.js
+++ util.js
@@ -1,10 +1,28 @@
--*- mode: Org -*-
+-*- mode: Org; eval: (progn (org-babel-do-load-languages 'org-babel-load-languages '((js . t)))) -*-
 
 * range
 
 #+begin_src js
   const range = a => [...Object.keys(Array(a).fill())].map(Number)
-  return range(0)
 #+end_src
 
-#+RESULTS:
+* tablefy
+
+turns Array[Object] into easily parsable table like structure
+
+| a | b | c |
+|---+---+---|
+| 1 |   |   |
+|   | 1 |   |
+|   |   | 1 |
+
+#+begin_src js
+  const tablefy = (obs) => {
+      const keys = [...new Set(obs.map(Object.keys).flat())]
+      return [keys
+  	    , ...obs
+  	    .map(a => keys.map(b => a[b]))
+  	   ]
+  }
+  // return tablefy([{a: 1}, {b: 1}, {c: 1}])
+#+end_src
